f6587a33408004ba6ea142081b16b45e4fc1104b
wuxiaojun <wuxiaojun@cmhi.chinamobile.com>
2018-08-17 15:39:31 +0800

    using zrender painter

--- src/GraphView.ts
+++ src/GraphView.ts
@@ -1,11 +1,16 @@
 import { Node, Edge } from './entities/';
 import zrender from 'zrender';
 
+const pic =  require('@/assets/images/icon.jpg');
+
+
 class GraphView {
   constructor(option: any) {
     // const node = new Node({});
     // node.render();
     const zr = zrender.init(document.getElementById('map') as HTMLDivElement);
+    const img = new Image();
+    img.src = pic;
     const circle = new zrender.Circle({
       shape: {
         cx: 150,
@@ -17,7 +22,27 @@ class GraphView {
         stroke: '#F00',
       },
     });
-    zr.add(circle);
+    const image = new zrender.Image({
+      draggable: true,
+      style: {
+        x: 300,
+        y: 300,
+        width: 150,
+        height: 150,
+        image: img,
+      }
+    });
+    const elem = document.getElementById('map') as HTMLCanvasElement;
+    const ctx =elem.getContext('2d');
+    const paniter = zr.painter;
+    paniter._doPaintEl(circle, {
+      ctx
+    }, true, {});
+    setTimeout(()=>{
+      paniter._doPaintEl(image, {
+        ctx
+      }, true, {});
+    }, 500);
   }
 }
 
